08528ce7d6af90ccc5afe0a7d29509a664285582
Tom <reslip@icloud.com>
2026-02-05 19:19:57 -0500

    fix comment headers

--- src/main/java/pi/logger/structs/SwerveModuleStateStruct.java
+++ src/main/java/pi/logger/structs/SwerveModuleStateStruct.java
@@ -1,5 +1,4 @@
-/**
- * //====================================================================================================================================================
+//====================================================================================================================================================
 // Copyright 2026 Lake Orion Robotics FIRST Team 302
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
@@ -13,7 +12,6 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 // OR OTHER DEALINGS IN THE SOFTWARE.
 //====================================================================================================================================================
- */
 package pi.logger.structs;
 
 import edu.wpi.first.util.struct.Struct;
